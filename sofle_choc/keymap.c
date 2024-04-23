f164255c49f3f1db533d0a276fb0fa0f77940b71
Abhijeet Rawat <abhirawat1304@gmail.com>
2024-04-23 22:53:17 +0530

    Update keymap.c

--- sofle_choc/keymap.c
+++ sofle_choc/keymap.c
@@ -20,7 +20,9 @@
 #define _SPCL  2
 
 enum custom_keycodes {
-  VIM_CMD = SAFE_RANGE
+  VIM_CMD = SAFE_RANGE,
+  VIM_QUIT,
+  VIM_SAVE
 };
 
 
@@ -36,17 +38,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      | LGUI | CMD/ | Space| / LALT  /       \ SPCL \  | Bspc | LCTRL|      |      |
+ *            |      | LGUI | CMD/ | Space| / LALT  /       \ CTRL \  | Bspc | SPCL |      |      |
  *            |      |      | CTRL |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
 
 [_WIN] = LAYOUT(
     KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    DF(_MAC),
-    KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_NUBS,
+    KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS,
     KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    _______,       _______,   KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                      _______, KC_LGUI, KC_LCTL, KC_SPC,  KC_LALT,       MO(_SPCL), KC_BSPC,  KC_LCTL,  _______, _______
+                      _______, KC_LGUI, KC_LCTL, KC_SPC,  KC_LALT,       KC_LCTL,   KC_BSPC,  MO(_SPCL),KC_DEL,  _______
 ),
 
 /*
@@ -60,17 +62,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      | LGUI | CMD/ | Space| / LALT  /       \ SPCL \  | Bspc | RCTRL|      |      |
+ *            |      | LGUI | CMD/ | Space| / LALT  /       \ CTRL \  | Bspc | SPCL |  DEL |      |
  *            |      |      | CTRL |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
 
 [_MAC] = LAYOUT(
     KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    DF(_WIN),
-    KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_NUBS,
+    KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS,
     KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    _______,       _______,   KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                      _______, KC_LGUI, KC_LCMD, KC_SPC,  KC_LALT,       MO(_SPCL), KC_BSPC,  KC_LCTL,  _______, _______
+                      _______, KC_LGUI, KC_LCMD, KC_SPC,  KC_LALT,       KC_LCTL,   KC_BSPC,  MO(_SPCL),KC_DEL,  _______
 ),
 
 /*
@@ -78,11 +80,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  F12 |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |  F10 | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |  (   |   -  |  )   |      |                    |   !  |   @  |   #  |   $  |   %  |  &   |
+ * |      |      |  (   |   +  |   )  |      |                    |   !  |   @  |   #  |   $  |   %  |  &   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   '  |  {   |   _  |   }  |   "  |-------.    ,-------| Left | Down |  Up  | Right|      |      |
+ * |      |   '  |  {   |   _  |   }  |  "   |-------.    ,-------| Left | Down |  Up  | Right|      |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |   =  |   [  |      |   ]  |  +   |-------|    |-------|      |      |      |      |      |      |
+ * |      |   =  |  [   |      |   ]  |  +   |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            |      |      |      |      | /       /       \      \  |      |      |      |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
@@ -90,10 +92,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_SPCL] = LAYOUT(
-    KC_F12,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F6,    KC_F7,    KC_F8,   KC_F9,    KC_F10,  KC_F11,
-    _______,  _______, KC_LPRN, KC_MINS, KC_RPRN, _______,                      KC_EXLM,  KC_AT,    KC_HASH, KC_DLR,   KC_PERC, KC_AMPR,
-    _______,  KC_QUOT, KC_LCBR, KC_UNDS, KC_RCBR, KC_DQT,                       KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, VIM_CMD, KC_CIRC,
-    _______,  KC_EQUAL,KC_LBRC, _______, KC_RBRC, KC_PLUS, _______,    _______, _______,  _______,  _______, _______,  _______, _______,
+    KC_F12,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F6,    KC_F7,    KC_F8,   KC_F9,    KC_F10,   KC_F11,
+    _______,  _______, KC_LPRN, KC_MINS, KC_RPRN, _______,                      KC_EXLM,  KC_AT,    KC_HASH, KC_DLR,   KC_PERC,  KC_AMPR,
+    _______,  KC_QUOT, KC_LCBR, KC_UNDS, KC_RCBR, KC_DQT,                       KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, VIM_SAVE, VIM_CMD,
+    _______,  KC_EQUAL,KC_LBRC, _______, KC_RBRC, KC_PLUS, _______,    _______, _______,  _______,  _______, _______,  _______,  VIM_QUIT,
                        _______, _______, _______, _______, _______,    _______, _______,  _______,  _______, _______
 )
 };
@@ -110,13 +112,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case VIM_CMD:
         if (record->event.pressed) {
-          register_code(KC_LCTL);
-          register_code(KC_C);
-          unregister_code(KC_LCTL);
-          unregister_code(KC_C);
+          tap_code16(LCTL(KC_C));
           tap_code16(KC_COLN);
           return 0;
         } 
+    case VIM_SAVE:
+        if (record->event.pressed) {
+          tap_code16(LCTL(KC_C));
+          send_string(":wa!\n");
+          return 0;
+        } 
+    case VIM_QUIT:
+        if (record->event.pressed) {
+          tap_code16(LCTL(KC_C));
+          send_string(":wqa!\n");
+          return 0;
+        } 
+
     }
     return 1;
 }
