93fb22d98e24cfc6340a92a1e6847fde249be435
Abhijeet Rawat <abhirawat1304@gmail.com>
2024-12-21 18:02:25 +0530

    fix

--- sofle_choc/keymap.c
+++ sofle_choc/keymap.c
@@ -15,17 +15,19 @@
  */
 #include QMK_KEYBOARD_H
 
-#define _WIN  0
-#define _MAC  1
-#define _SPCL  2
+#define _WIN   0
+#define _SPCL  1
 
 enum custom_keycodes {
   VIM_CMD = SAFE_RANGE,
   VIM_QUIT,
-  VIM_SAVE
+  VIM_SAVE,
+  CMDCTL,
+  SW_CMDCTL,
 };
 
 
+uint16_t CMDCTL_VAL = KC_LCTL;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * QWERTY
@@ -44,35 +46,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_WIN] = LAYOUT(
-    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    DF(_MAC),
-    KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS,
-    KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    _______,       _______,   KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                      _______, KC_LGUI, KC_LCTL, KC_SPC,  KC_LALT,       MO(_SPCL), KC_BSPC,  KC_RCTL,  KC_DEL,  _______
-),
-
-/*
- * QWERTY
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | MODE |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | Enter|
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      | LGUI | CMD  | Space| / LALT  /       \ CTRL \  | Bspc | SPCL |  DEL |      |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
- *            `----------------------------------'           '------''---------------------------'
- */
-
-[_MAC] = LAYOUT(
-    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    DF(_WIN),
-    KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS,
-    KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    _______,       _______,   KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                      _______, KC_LGUI, KC_LCMD, KC_SPC,  KC_LALT,       MO(_SPCL), KC_BSPC,  KC_RCTL,  KC_DEL,  _______
+    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,   KC_9,   KC_0,    SW_CMDCTL,
+    KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,   KC_O,   KC_P,    KC_BSLS,
+    KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,   KC_L,   KC_SCLN, KC_ENT,
+    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   _______,       _______,   KC_N,    KC_M,    KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
+                      _______, KC_LGUI, CMDCTL, KC_SPC,  KC_LALT,       MO(_SPCL), KC_BSPC, KC_LCTL, KC_DEL, _______
 ),
 
 /*
@@ -104,7 +82,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
     { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
-    { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
 };
 #endif
 
@@ -126,6 +103,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           return false;
         } 
         break;
+    case SW_CMDCTL:
+        if (record->event.pressed) {
+          if (CMDCTL_VAL == KC_LCTL) {
+            CMDCTL_VAL = KC_LCMD;
+          } else {
+            CMDCTL_VAL = KC_LCTL;
+          }
+        }
+    case CMDCTL:
+        if (record->event.pressed) {
+          register_code(CMDCTL_VAL);
+        } else {
+          unregister_code(CMDCTL_VAL);
+        } 
+        break;
     }
     return true;
 }
+
